1f5795e9b931c00b2edea2b70162de80cc3b49f0
Navid Dezashibi <dezashibi@gmail.com>
2020-08-14 18:36:45 +0430

    added admin parts and customized admin page to fit the whole system

--- app/listings/models.py
+++ app/listings/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from realtors.models import Realtor
+from datetime import datetime
 
 class Listing(models.Model):
     """Handle all listings data in database"""
@@ -24,4 +25,7 @@ class Listing(models.Model):
     photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
-    list_date = models.DateTimeField(auto_now_add=True, blank=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
--- app/realtors/admin.py
+++ app/realtors/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class RealtorAdmin(admin.ModelAdmin):
+    """Handles all admin page customization for realtors"""
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    list_editable = ('email',)
+    search_fields = ('name', 'email')
+    list_per_page = 25
+
+admin.site.register(models.Realtor, RealtorAdmin)
